5e14e275726ea316a7f29d311eaecb24cbf9f7e4
Brent Bailey & Garrett Olson <student@epicodus.com>
2015-05-28 16:21:40 -0700

    sort output by occurence

--- js/scripts.js
+++ js/scripts.js
@@ -12,5 +12,50 @@ var wordCount = function(phrase) {
     }
   });
 
-  return wordCounts;
+  return sortHash(wordCounts);
 };
+
+var sortHash = function(hash) {
+  var sortable = [];
+
+  for (var key in hash) {
+    sortable.push([key, hash[key]]);
+  }
+
+  sortable.sort(function(a, b) {
+    return a[1] < b[1];
+  });
+
+  return sortable;
+};
+
+
+// var sortHash = function(hash) {
+//   var keyArray = []
+//   var keyValueArray = []
+//   for (var key in hash) {
+//     keyArray.push(key);
+//   }
+//
+//   keyArray.forEach(function(key) {
+//     if (hash[key] > hash[keyArray[0]]) {
+//     var keyIndex = keyArray.indexOf(key)
+//     keyArray.splice(keyIndex, 1);
+//     keyArray.unshift(key);
+//   }
+//   });
+//   keyArray.forEach(function(key)  {
+//     keyValueArray.push([key, hash[key]])
+//   });
+console.log(keyArray)
+  return keyValueArray;
+};
+
+$(function() {
+  $("form").submit(function() {
+
+    $("#result-wrapper").show();
+    $("#result").text(wordCount($("input").val()));
+    return false;
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,14 @@
 describe("wordCount", function() {
   it("returns the count of a single word", function() {
-    expect(wordCount("kitten kitten")).to.eql({"kitten": 2});
+    expect(wordCount("kitten kitten")).to.eql([["kitten", 2]]);
   });
 
   it("returns counts for a variety of words", function() {
-    expect(wordCount("kitten kitten puppie hippo kitten puppie")).to.eql({"kitten": 3, "puppie": 2, "hippo": 1});
+    expect(wordCount("kitten kitten puppie hippo kitten puppie")).to.eql([["kitten", 3], ["puppie", 2], ["hippo", 1]]);
+  });
+
+  it("returns counts for a complex case", function() {
+    expect(wordCount("one three three three two two four four four four six six six six six six five five five five five"))
+      .to.eql([["six", 6], ["five", 5], ["four", 4], ["three", 3], ["two", 2], ["one", 1]]);
   });
 });
